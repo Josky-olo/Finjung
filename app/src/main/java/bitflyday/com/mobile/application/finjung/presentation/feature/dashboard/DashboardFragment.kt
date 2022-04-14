28ad016dcfb74a2bdd09053cde0a1e1763b0008a
Josky-olo <56986562+Josky-olo@users.noreply.github.com>
2022-04-14 15:35:32 +0700

    Added Motion toast by using Spikeysanju libraries dependency.

--- app/src/main/java/bitflyday/com/mobile/application/finjung/presentation/feature/dashboard/DashboardFragment.kt
+++ app/src/main/java/bitflyday/com/mobile/application/finjung/presentation/feature/dashboard/DashboardFragment.kt
@@ -12,6 +12,9 @@ import androidx.core.view.updatePadding
 import androidx.fragment.app.viewModels
 import bitflyday.com.mobile.application.finjung.R
 import bitflyday.com.mobile.application.finjung.databinding.FragmentDashboardBinding
+import bitflyday.com.mobile.application.finjung.utilities.showToast
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
 
 
 class DashboardFragment : Fragment() {
@@ -23,10 +26,24 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDashboardBinding.inflate(inflater,container,false).apply {
+        binding = FragmentDashboardBinding.inflate(inflater, container, false).apply {
             viewModel = dashboardViewModel
         }
+        setToastColors()
+        showToast(MotionToastStyle.ERROR, "Hello world", "How are you.", requireActivity())
         binding.toolbar.navigationIcon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_clapperboard_svgrepo_com)
         return binding.root
+    }
+    private fun setToastColors() {
+            MotionToast.setSuccessColor(R.color.custom_success_color)
+            MotionToast.setErrorColor(R.color.Red)
+            MotionToast.setDeleteColor(R.color.custom_delete_color)
+            MotionToast.setWarningColor(R.color.custom_warning_color)
+            MotionToast.setInfoColor(R.color.Primary)
+            MotionToast.setSuccessBackgroundColor(R.color.success_bg_color)
+            MotionToast.setErrorBackgroundColor(R.color.error_bg_color)
+            MotionToast.setDeleteBackgroundColor(R.color.delete_bg_color)
+            MotionToast.setWarningBackgroundColor(R.color.warning_bg_color)
+            MotionToast.setInfoBackgroundColor(R.color.info_bg_color)
     }
 }
--- app/src/main/java/bitflyday/com/mobile/application/finjung/utilities/UiUntils.kt
+++ app/src/main/java/bitflyday/com/mobile/application/finjung/utilities/UiUntils.kt
@@ -1,11 +1,17 @@
 package bitflyday.com.mobile.application.finjung.utilities
 
+import android.app.Activity
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import bitflyday.com.mobile.application.finjung.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
 
 inline fun Fragment.launchAndRepeatWithViewLifecycle(
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
@@ -17,3 +23,14 @@ inline fun Fragment.launchAndRepeatWithViewLifecycle(
         }
     }
 }
+
+fun showToast(style: MotionToastStyle, title: String, message: String, activity: Activity) {
+    MotionToast.createColorToast(
+        activity, title,
+        message,
+        style,
+        MotionToast.GRAVITY_BOTTOM,
+        MotionToast.ToastDuration.LONG_DURATION,
+        ResourcesCompat.getFont(activity, R.font.helvetica_regular)
+    )
+  }
